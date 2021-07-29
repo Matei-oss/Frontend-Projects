459c3c1567aa002760df3a6697fd39f206fbec9c
MATEIU <IMATEIU@steelcase.com>
2021-07-29 10:35:23 +0300

    added functionality to choose winner

--- Frontend-JS/Dicee Challenge/index.js
+++ Frontend-JS/Dicee Challenge/index.js
@@ -3,3 +3,12 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector("img.img1").setAttribute("src", `images/dice${randomNumber1}.png`);
 document.querySelector("img.img2").setAttribute("src", `images/dice${randomNumber2}.png`);
+
+
+if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+} else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
