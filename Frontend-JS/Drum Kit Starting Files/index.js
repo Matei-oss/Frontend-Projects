b90ea314e94d09a3d581a375bc88bb76cc9f3f7b
MATEIU <IMATEIU@steelcase.com>
2021-07-29 13:46:02 +0300

    remade javascript file

--- Frontend-JS/Drum Kit Starting Files/index.js
+++ Frontend-JS/Drum Kit Starting Files/index.js
@@ -4,7 +4,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-        var buttonInnerHTML = this.InnerHTML;
+        var buttonInnerHTML = this.innerHTML;
 
         switch (buttonInnerHTML) {
             case "w":
@@ -18,32 +18,34 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
                 break;
 
             case "s":
-                var tom3 = new Audio("sounds/tom-3.mp3");
+                var tom3 = new Audio('sounds/tom-3.mp3');
                 tom3.play();
                 break;
 
             case "d":
-                var tom4 = new Audio("sounds/tom-4.mp3");
+                var tom4 = new Audio('sounds/tom-4.mp3');
                 tom4.play();
                 break;
 
             case "j":
-                var snare = new Audio("sounds/snare.mp3");
+                var snare = new Audio('sounds/snare.mp3');
                 snare.play();
                 break;
 
             case "k":
-                var crash = new Audio("sounds/crash.mp3");
+                var crash = new Audio('sounds/crash.mp3');
                 crash.play();
                 break;
 
             case "l":
-                var kick = new Audio("sounds/kick-bass.mp3");
+                var kick = new Audio('sounds/kick-bass.mp3');
                 kick.play();
                 break;
 
+
             default:
-                console.log(buttonInnerHTML);
+                console.log(key);
+
         }
 
     });
