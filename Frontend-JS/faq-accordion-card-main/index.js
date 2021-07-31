02c002aac864897b359b8f139c48ed38ccc2bc6a
MATEIU <IMATEIU@steelcase.com>
2021-07-31 12:21:05 +0300

    refined jQuery, added new class

--- Frontend-JS/faq-accordion-card-main/index.js
+++ Frontend-JS/faq-accordion-card-main/index.js
@@ -1,3 +1,8 @@
-$('.accordion-button').click(function(e) {
-    $(this).css('font-weight', '700');
+// $('.accordion-button').click(function(e) {
+//     $(this).css('font-weight', '700');
+// });
+
+$(".accordion-button").on("click", function() { // attach to Click event
+    $(".accordion-button").removeClass("active"); // reset all <li> to no active class
+    $(this).addClass("active"); // add active class to this <li> only
 });
