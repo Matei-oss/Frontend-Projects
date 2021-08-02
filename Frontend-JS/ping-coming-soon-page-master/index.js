56dacff0809c9422ce43b4619362f258a04ef362
MATEIU <IMATEIU@steelcase.com>
2021-08-02 17:20:46 +0300

    added jquery script for email validation

--- Frontend-JS/ping-coming-soon-page-master/index.js
+++ Frontend-JS/ping-coming-soon-page-master/index.js
@@ -1,26 +1,8 @@
-$(document).ready(function() {
-
-    $('.btn').click(function() {
-
-        $(".error").hide();
-        var hasError = false;
-        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-        var emailblockReg =
-            /^([\w-\.]+@(?!gmail.com)(?!yahoo.com)(?!hotmail.com)([\w-]+\.)+[\w-]{2,4})?$/;
-
-        var emailaddressVal = $("#UserEmail").val();
-        if (emailaddressVal == '') {
-            $("#UserEmail").after('<span class="error">Please enter your email address.</span>');
-            hasError = true;
-        } else if (!emailReg.test(emailaddressVal)) {
-            $("#UserEmail").after('<span class="error">Enter a valid email address.</span>');
-            hasError = true;
-        } else if (!emailblockReg.test(emailaddressVal)) {
-            $("#UserEmail").after('<span class="error">No yahoo, gmail or hotmail emails.</span>');
-            hasError = true
+$(".form-test").validate({
+    rules: {
+        emailfield: {
+            required: true,
+            email: true
         }
-
-        if (hasError == true) { return false; }
-
-    });
+    }
 });
