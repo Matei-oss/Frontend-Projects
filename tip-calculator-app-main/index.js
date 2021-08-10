ca4e941d3866511e0abfbdce2b8cae29651b0e98
MATEIU <IMATEIU@steelcase.com>
2021-08-10 15:14:33 +0300

    added functionality to mantain button color

---
+++ tip-calculator-app-main/index.js
@@ -0,0 +1,9 @@
+$('.btn.tip-percent').click(function() {
+    $('.btn.tip-percent').removeClass('onclick')
+    $(this).addClass('onclick');
+});
+
+$('.btn.tip-percent-r').click(function() {
+    $('.btn.tip-percent-r').removeClass('onclick')
+    $(this).addClass('onclick');
+});
