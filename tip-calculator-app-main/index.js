7e868347a57673603b5abd8dc24a677e7bb82b4a
MATEIU <IMATEIU@steelcase.com>
2021-08-10 17:19:46 +0300

    added tip amount per person functionality

--- tip-calculator-app-main/index.js
+++ tip-calculator-app-main/index.js
@@ -7,3 +7,22 @@ $('.btn.tip-percent-r').click(function() {
     $('.btn.tip-percent-r').removeClass('onclick')
     $(this).addClass('onclick');
 });
+
+
+
+function getPercentage() {
+    $(".btn").click(function() {
+        var tipPercentage = parseInt($(this).val());
+        console.log(tipPercentage)
+        return tipPercentage;
+    })
+};
+
+
+$('.amount-input.input-person').keyup(function(tipPercentage) {
+    var billAmount = parseInt($('.amount-input.input-dollar').val());
+    var numberOfPeople = parseInt($('.amount-input.input-person').val());
+
+    var tipAmount = ((billAmount / 100) * 10) / numberOfPeople;
+    $('.result')[0].innerHTML = "$" + tipAmount;
+});
