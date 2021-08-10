ae7f80b4b43c8872d99207ae4e36b6b7fe8102fb
MATEIU <IMATEIU@steelcase.com>
2021-08-10 20:57:41 +0300

    corrected tip calculation function

--- tip-calculator-app-main/index.js
+++ tip-calculator-app-main/index.js
@@ -1,28 +1,27 @@
+//change color of percent buttons after click
 $('.btn.tip-percent').click(function() {
     $('.btn.tip-percent').removeClass('onclick')
     $(this).addClass('onclick');
 });
 
+//change color of reset button after click
 $('.btn.tip-percent-r').click(function() {
     $('.btn.tip-percent-r').removeClass('onclick')
     $(this).addClass('onclick');
 });
 
-
-
-function getPercentage() {
-    $(".btn").click(function() {
-        var tipPercentage = parseInt($(this).val());
-        console.log(tipPercentage)
-        return tipPercentage;
-    })
-};
-
-
-$('.amount-input.input-person').keyup(function(tipPercentage) {
+//calculate tip amounts per person and total
+$('.amount-input.input-person,.btn').on('click', function() {
+    var tipPercentage = parseInt($(this).val());
     var billAmount = parseInt($('.amount-input.input-dollar').val());
     var numberOfPeople = parseInt($('.amount-input.input-person').val());
-
-    var tipAmount = ((billAmount / 100) * 10) / numberOfPeople;
+    var tipAmount = Math.floor(((billAmount / 100) * tipPercentage) / numberOfPeople);
+    var tipTotal = Math.floor(tipAmount * numberOfPeople);
     $('.result')[0].innerHTML = "$" + tipAmount;
+    $('.result')[1].innerHTML = "$" + tipTotal;
+});
+
+//reloads page when reset button is clicked
+$('.tip-percent-r').click(function() {
+    location.reload();
 });
